ecfd5fd7c9233e0d8d00c043ad81a0df0d0c72a8
Priyanshu Sinha <Priyanshu_Sinha@outlook.com>
2022-04-22 10:29:54 -0400

    Minor fixes in chest-xay-classification. Added FP32 QAT weights.
    Signed-off-by: Priyanshu Sinha <Priyanshu_Sinha@outlook.com>

--- chext_xray_classification/src/eval_FP32_thermal.py
+++ chext_xray_classification/src/eval_FP32_thermal.py
@@ -7,23 +7,66 @@ from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 
-DATASET_PATH = os.path.abspath('../dataset/')
+import tensorflow_model_optimization as tfmot
+
+quantize_annotate_layer = tfmot.quantization.keras.quantize_annotate_layer
+quantize_annotate_model = tfmot.quantization.keras.quantize_annotate_model
+quantize_scope = tfmot.quantization.keras.quantize_scope
+
+class DefaultBNQuantizeConfig(tfmot.quantization.keras.QuantizeConfig):
+    def get_weights_and_quantizers(self, layer):
+        return []
+    
+    def get_activations_and_quantizers(self, layer):
+        return []
+    
+    def set_quantize_weights(self, layer, quantize_weights):
+        pass
+
+    def set_quantize_activations(self, layer, quantize_activations):
+        pass
+
+    def get_output_quantizers(self, layer):
+        return [tfmot.quantization.keras.quantizers.MovingAverageQuantizer(
+    num_bits=8, per_axis=False, symmetric=False, narrow_range=False)]
+
+    def get_config(self):
+        return {}
+    
+def apply_quantization_to_batch_normalization(layer):
+    if isinstance(layer, tf.keras.layers.BatchNormalization):
+        return quantize_annotate_layer(layer, DefaultBNQuantizeConfig())
+    
+    return layer
+
+DATASET_PATH = os.path.abspath('/data/NIH_14')
 
 data, all_labels = get_data(DATASET_PATH)
 _, _, test_df = split_train_dev_test(data)
 test_gen = get_data_generator(test_df, all_labels)
 
-model = get_model()
+model = get_model(all_labels)
+
+annotated_model = tf.keras.models.clone_model(
+                    model,
+                    clone_function=apply_quantization_to_batch_normalization,
+)
+
+with quantize_scope(
+  {'DefaultBNQuantizeConfig': DefaultBNQuantizeConfig}):
+  # Use `quantize_apply` to actually make the model quantization aware.
+  quant_aware_model = tfmot.quantization.keras.quantize_apply(annotated_model)
 
 initial_learning_rate=1e-3
 optimizer = Adam(lr=initial_learning_rate)
-model.compile(optimizer=optimizer, loss="binary_crossentropy")
+quant_aware_model.compile(optimizer=optimizer, loss="binary_crossentropy")
 
-model.load_weights('../weights/FP_32_QAT_weights.h5')
+quant_aware_model.load_weights('../weights/FP_32_QAT_weights.h5')
 
 count = 0
 while True:
-    model.predict_generator(test_gen, steps=test_gen.n/test_gen.batch_size, verbose = True)
+    output = quant_aware_model.predict_generator(test_gen, steps=test_gen.n/test_gen.batch_size, verbose = True)
+    print(f"Predicted Output : {output}")
     count += 1
     if count == 100000:
         break
---
+++ chext_xray_classification/src/eval_INT8_thermal.py
@@ -0,0 +1,63 @@
+import os
+
+from get_data import *
+import tensorflow as tf
+import tensorflow.keras.backend as K
+import sys
+
+
+def eval_tflite(interpreter, test_data):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    interpreter.resize_tensor_input(input_details[0]['index'], (32, 224, 224, 3))
+    interpreter.resize_tensor_input(output_details[0]['index'], (32, 14))
+    input_details = interpreter.get_input_details()[0]
+    output_details = interpreter.get_output_details()
+    print(f"Input Details : {input_details}")
+    print(f"Output Details : {output_details}")
+    interpreter.allocate_tensors()
+    test_data.reset()
+    for x, y in test_data:
+        x_img = x.astype(input_details["dtype"])
+        interpreter.set_tensor(input_details['index'], x_img)
+        interpreter.invoke()
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        print(f"Output Data : {output_data}")
+
+def eval_int8(test_data):
+    INT8_MODEL_PATH = os.path.abspath('../weights/QAT_INT8.tflite')
+    interpreter = tf.lite.Interpreter(model_path = INT8_MODEL_PATH)
+    return eval_tflite(interpreter, test_data)
+    
+
+def eval_int8_edge_tpu(test_data, n):
+    INT8_MODEL_PATH = os.path.abspath('../weights/QAT_INT8_edgetpu.tflite')
+    interpreter = tf.lite.Interpreter(model_path = INT8_MODEL_PATH, experimental_delegates=[tf.lite.load_delegate('libedgetpu.so.1')])
+    return eval_tflite(interpreter, test_data, n)
+
+
+if __name__ == "__main__":
+    DATASET_PATH = os.path.abspath('/data/NIH_14')
+
+    data, all_labels = get_data(DATASET_PATH)
+    _, _, test_df = split_train_dev_test(data)
+    test_gen = get_data_generator(test_df, all_labels)
+    
+    if int(sys.argv[1]) == 0:
+        print(f"Evaluating INT 8 Model : ")
+        count = 0
+        while True:
+            eval_int8(test_gen)
+            count += 1
+            if count == 100000:
+                break
+            
+    elif int(sys.argv[1]) == 1:
+        print(f"Evaluating Edge TPU Compiled INT8 Model : ")
+        count = 0
+        while True:
+            eval_int8_edge_tpu(test_gen)
+            count += 1
+            if count == 100000:
+                break
+        
